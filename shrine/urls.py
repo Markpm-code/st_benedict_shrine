fdd8af51d5344c080ce9b453ee207c73e4bffef5
Markpm-code <marlonseglos@yahoo.com>
2023-02-08 11:08:18 +0000

    rename about.html and change update urls path

--- shrine/urls.py
+++ shrine/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index.html'),
-     path('about/',views.about, name='about.html'),
+    path('our_story/',views.our_story, name='our_story.html'),
     path('gallery/',views.gallery, name='gallery.html'),
     path('services/', views.services, name='services.html')
 ]
--- shrine/views.py
+++ shrine/views.py
@@ -5,8 +5,8 @@ def index(request):
     return render(request, 'index.html')
 
 
-def about(request):
-    return render(request, 'about.html')
+def our_story(request):
+    return render(request, 'our_story.html')
 
 
 def gallery(request):
