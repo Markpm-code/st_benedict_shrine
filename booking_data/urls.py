dc57da8d57357fd79a338d1376adcffbf102e433
Markpm-code <marlonseglos@yahoo.com>
2023-02-23 18:31:38 +0000

    add amend_booking.html file

--- booking_data/urls.py
+++ booking_data/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('makebooking', views.booking, name='booking'),
     path('reservations/', views.ReservationList.as_view(), name='reservations'),
+    path('amend/<reservation_id>', views.amend_reservation, name='amend'),
+    path('cancel/<reservation_id>', views.cancel_reservation, name='cancel'),
 ]
