1597aa17d87f26873531ded7544eb4ff20d6a2c9
Markpm-code <>
2023-04-29 22:41:24 +0000

    Edit allowed host

--- st_benedict_shrine/settings.py
+++ st_benedict_shrine/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-markpm-code-st-benedict--9sula8ff9k.us2.codeanyapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://st-benedict.herokuapp.com/', 'localhost']
 
 
 # Application definition
