e4f9d0771ba6a3a9e55fcb677838631aa655ce71
Markpm-code <marlonseglos@yahoo.com>
2023-06-23 10:56:37 +0000

    Fixed long characters error

--- booking_data/urls.py
+++ booking_data/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('makebooking', views.booking, name='booking'),
-    path('reservations/', views.BookingReservationList.as_view(), name='reservations'),
-    path('amend/<reservation_id>', views.AmendBookingReservationList, name='amend'),
+    path('reservations/',
+         views.BookingReservationList.as_view(), name='reservations'),
+    path('amend/<reservation_id>',
+         views.AmendBookingReservationList, name='amend'),
     path('cancel/<reservation_id>', views.cancel_reservation, name='cancel'),
 ]
