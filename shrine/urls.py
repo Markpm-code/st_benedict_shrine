1893fcd8ddbad8e37252c549068584cd33375423
Markpm-code <marlonseglos@yahoo.com>
2023-06-23 12:20:44 +0000

    Added missing whitespaces in shrine urls

--- shrine/urls.py
+++ shrine/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index.html'),
-    path('our_story/',views.our_story, name='our_story.html'),
-    path('gallery/',views.gallery, name='gallery.html'),
+    path('our_story/', views.our_story, name='our_story.html'),
+    path('gallery/', views.gallery, name='gallery.html'),
     path('services/', views.services, name='services.html'),
 ]
