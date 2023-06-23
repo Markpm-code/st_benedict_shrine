68b108e8947d8b5cc0d0d2dc65488578c4fd3cee
Markpm-code <marlonseglos@yahoo.com>
2023-06-23 12:18:21 +0000

    Removed trailing whitespaces in shrine views

--- shrine/views.py
+++ shrine/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -11,8 +12,8 @@ def our_story(request):
 
 
 def gallery(request):
-    return render(request, 'gallery.html') 
+    return render(request, 'gallery.html')
 
 
 def services(request):
-    return render(request, 'services.html') 
+    return render(request, 'services.html')
